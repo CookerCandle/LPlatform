c595780e25458ebddb27a4cc2c3ec74843945882
CookerCandle <jamoliddingafurov3223@gmail.com>
2025-09-17 02:31:27 +0500

    removed class maked func and navigate data and users on click

--- src/components/Lessons.jsx
+++ src/components/Lessons.jsx
@@ -1,38 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdPlayLesson } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 
 
-class Lessons extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            lessons: [
-                { lesson: 1, name: "Математика", description: "Высшая математика" },
-                { lesson: 2, name: "Программирование", description: "Программирование на языке C++" },
-                { lesson: 3, name: "Черчения", description: "Черчения высоких зданий" }
-            ]
-        };
-    }
-    render() {
-        return(
-            <div className='container mt-4'>
-                <h2>Список уроков</h2>
-                <ul className='list-group'>
-                    {this.state.lessons.map(item => (
-                        <li key={item.lesson} className="list-group-item d-flex align-items-center mt-3 li-lesson">
-                            <MdPlayLesson size={24} className="me-2" />
-                            <div>
-                                <strong>{item.name}</strong>
-                                <p className='mb-0'>{item.description}</p>
-                            </div>
-                        </li>
-                    ))}
 
-                </ul>
+const Lessons = () => {
+    const navigate = useNavigate();
+    const [lessons] = useState([
+            { lesson: 1, name: "Математика", description: "Высшая математика", url: "mathematic" },
+            { lesson: 2, name: "Программирование", description: "Программирование на языке C++", url: "programming" },
+            { lesson: 3, name: "Химия", description: "Химия рективных элементов", url: "alchemy" }
+    ]) 
+    return(
+        <div className='container mt-4'>
+            <h2>Список уроков</h2>
+            <ul className='list-group'>
+                {lessons.map(item => (
+                    <li key={item.lesson} className="list-group-item d-flex align-items-center mt-3 li-lesson" onClick={() => navigate(`${item.url}`, {state: item})}>
+                        <MdPlayLesson size={24} className="me-2" />
+                        <div>
+                            <strong>{item.name}</strong>
+                            <p className='mb-0'>{item.description}</p>
+                        </div>
+                    </li>
+                ))}
 
-            </div>
-        );
-    }
+            </ul>
+
+        </div>
+    );
 }
 
 export default Lessons
