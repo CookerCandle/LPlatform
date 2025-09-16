7b8f8e53d347c2803e59d17502644b0e33d3828b
CookerCandle <jamoliddingafurov3223@gmail.com>
2025-09-17 02:30:36 +0500

    lessonlayout

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 
 import MainLayout from './layouts/MainLayout';
 import LessonLayout from './layouts/LessonLayout';
+
 import HomePage from './pages/HomePage';
 import NotFound from './pages/NotFound';
 import Learning from './pages/Learning';
 import Profile from './pages/Profile';
 import Login from './pages/Login';
-import Test from './pages/Test'
+import Lesson from './pages/Lesson';
 
 class App extends React.Component {
   render() {
@@ -26,7 +27,7 @@ class App extends React.Component {
 
         {/* Под страницы */}
         <Route element={<LessonLayout />} >
-          <Route path="Learning/:param" element={<Test />} />
+          <Route path="Learning/:param" element={<Lesson />} />
         </Route>
 
         {/* 404 */}
