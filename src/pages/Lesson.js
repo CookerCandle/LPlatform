0653c0b2e873339c59b116aaf45c9befa1e94d48
cooker <jamoliddingafurov3223@gmail.com>
2025-09-17 16:44:58 +0500

    check url get units

--- src/pages/Lesson.js
+++ src/pages/Lesson.js
@@ -1,16 +1,30 @@
-import React from "react";
-import { useParams, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+import lessonsData from "../data/lessonsData";
+import Unit from "../components/Unit";
 
 function Lesson() {
+    const navigate = useNavigate();
     const { param } = useParams();
-    const location = useLocation();
-    const lesson = location.state;
+    const [lessons] = useState(lessonsData);
+    const lesson = lessons[param];
+
+    if (!lesson) {
+        navigate("/Learning");
+        return null;
+    }
 
     return (
         <div className="container mt-4">
-            <h2>Страница урока</h2>
-            <p>Вы зашли через параметр: <strong>{param}</strong></p>
-            <p>{lesson.name}</p>
+            <h2>{lesson.name}</h2>
+            <ul className="list-group">
+                {lesson.units.map((unit, index) => (
+                    <li key={index} className="list-group-item d-flex align-items-center mt-3 li-lesson">
+                        <Unit unit={unit} />
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
